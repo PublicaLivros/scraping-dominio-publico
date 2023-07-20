6cad048376f4854fbfeffa73b767854bc57df2db
Matheus Bastos <matheusmcy1@gmail.com>
2023-07-20 14:05:05 -0300

    Script remove raw_data or books from booklibrary.

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+feedback="init"
+
 function execute() {
     local executecommand=$1
     if [[ executecommand -eq 1 ]]; then
@@ -7,10 +9,18 @@ function execute() {
         node ./js/scraping.js
     elif [[ executecommand -eq 2 ]]; then
         echo "Downloading the books using the obtained scraping..."
-        if [[ -e "./json/raw_data.json" ]]; then
-            python ./python/download.py
-        else
-            echo "Oops, you didn't do the scraping!"
+        if [[ -e "./json/raw_data.json" ]]; then python ./python/download.py;
+        else echo "Oops, you didn't do the scraping!";
+        fi
+    elif [[ executecommand -eq 3 ]]; then
+        echo "Removing raw_data.json..."
+        if [[ -e "./json/raw_data.json" ]]; then rm ./json/raw_data.json;
+        else echo "Scraping data doesn't exist";
+        fi
+    elif [[ executecommand -eq 4 ]]; then
+        echo "Removing all books from booklibrary..."
+        if [[ -d "./booklibrary" ]]; then rm ./booklibrary/*;
+        else echo "There are no books saved here.";
         fi
     else
         return 1
@@ -19,13 +29,18 @@ function execute() {
 
 function showMenu() {
     echo -e "SCRAPPING - PUBLIC DOMAIN: 
-1) NodeJS scrapping for raw_data.
-2) Download books from raw_data scrapping.
+1) NodeJS scraping for raw_data.
+2) Download books from raw_data scraping.
+3) Remove raw_data scraping.
+4) Remove all books from "booklibrary".
 help) Help about this script.
 "
-echo -n "Choose: "
-read -r usrinput
-testInput $usrinput
+    echo "$feedback"
+    echo -n "Choose: "
+    read -r usrinput
+    clear
+    testInput $usrinput
+    feedback=""
 }
 
 function testInput() {
@@ -36,12 +51,19 @@ function testInput() {
         2) 
             execute 2
         ;;
+        3) 
+            execute 3
+        ;;
+        4) 
+            execute 4
+        ;;
         help)
             echo "Is it so complicated to press a button?"
         ;;
         *)
             clear
-            echo "Command not found."
+            if [[ $feedback != "init" ]]; then feedback="Command not found."; fi
+            if [[ $feedback == "init" ]]; then feedback=""; fi
             showMenu
         ;;
     esac
